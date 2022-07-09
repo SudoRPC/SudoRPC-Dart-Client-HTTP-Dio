3fc0efab115ca41ef53e05db72ee758b8b5b67b3
wmxpy <wm@mengw.io>
2022-07-09 18:28:41 -0500

    init dart file

--- example/sudorpc_client_http_dio_example.dart
+++ example/sudorpc_client_http_dio_example.dart
@@ -1,6 +1,3 @@
-import 'package:sudorpc_client_http_dio/sudorpc_client_http_dio.dart';
-
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  print('WIP');
 }
--- test/sudorpc_client_http_dio_test.dart
+++ test/sudorpc_client_http_dio_test.dart
@@ -1,16 +1,9 @@
-import 'package:sudorpc_client_http_dio/sudorpc_client_http_dio.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
     setUp(() {
       // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
     });
   });
 }
