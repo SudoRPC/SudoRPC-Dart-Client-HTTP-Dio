9509b5cc0df31318445293b6519c843f6c352dd1
wmxpy <wm@mengw.io>
2022-07-10 22:24:42 -0500

    init dio proxy

--- lib/src/dio_proxy.dart
+++ lib/src/dio_proxy.dart
@@ -1,1 +1,28 @@
+import 'package:sudorpc/sudorpc.dart';
 
+class SudoRPCDioHTTPProxy implements SudoRPCCallProxy {
+  @override
+  void addListener(
+    String listenerIdentifier,
+    void Function(
+      SudoRPCReturn message,
+    )
+        callback,
+  ) {
+    // TODO: implement addListener
+  }
+
+  @override
+  void removeListener(
+    String listenerIdentifier,
+  ) {
+    // TODO: implement removeListener
+  }
+
+  @override
+  void send(
+    SudoRPCCall call,
+  ) {
+    // TODO: implement send
+  }
+}
