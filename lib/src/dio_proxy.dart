7b86e0f3dc990e1c8f590d580ce224415dc63163
wmxpy <wm@mengw.io>
2022-07-10 22:43:51 -0500

    add dio proxy

--- lib/src/dio_proxy.dart
+++ lib/src/dio_proxy.dart
@@ -1,13 +1,25 @@
+import 'package:dio/dio.dart';
 import 'package:sudorpc/sudorpc.dart';
+import 'package:sudorpc_client_http_dio/sudorpc_client_http_dio.dart';
 
 class SudoRPCDioHTTPProxy implements SudoRPCCallProxy {
+  final String endpoint;
+
+  final BaseOptions? baseOptions;
+  final List<InterceptorsWrapper>? interceptors;
+
+  final Map<String, SudoRPCCallProxyCallback> _listeners = {};
+
+  SudoRPCDioHTTPProxy({
+    required this.endpoint,
+    this.baseOptions,
+    this.interceptors,
+  });
+
   @override
   void addListener(
     String listenerIdentifier,
-    void Function(
-      SudoRPCReturn message,
-    )
-        callback,
+    SudoRPCCallProxyCallback callback,
   ) {
     // TODO: implement addListener
   }
@@ -23,6 +35,39 @@ class SudoRPCDioHTTPProxy implements SudoRPCCallProxy {
   void send(
     SudoRPCCall call,
   ) {
-    // TODO: implement send
+    final Dio dio = getSudoRPCDio(
+      baseOptions: baseOptions,
+      interceptors: interceptors,
+    );
+
+    dio.post(endpoint, data: call.toJson()).then(
+      (Response<dynamic> response) {
+        for (final listener in _listeners.values) {
+          listener(createSudoRPCReturnFromJson(response.data));
+        }
+      },
+    ).catchError(
+      (error) {
+        if (call is! SudoRPCCallV1) {
+          throw Exception('SudoRPCDioHTTPProxy only supports SudoRPCCallV1');
+        }
+
+        final SudoRPCReturnV1FailErrorItem errorItem =
+            SudoRPCReturnV1FailErrorItem(
+          error: 'Dio Error',
+          message: error.toString(),
+          result: error,
+        );
+
+        final SudoRPCReturn errorReturn = SudoRPCReturnV1Fail(
+          identifier: call.identifier,
+          errors: [errorItem],
+        );
+
+        for (final listener in _listeners.values) {
+          listener(errorReturn);
+        }
+      },
+    );
   }
 }
